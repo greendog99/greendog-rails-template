7addc387a9de4cc40f4ec257b790ff4577c6ac30
Mike Fischer <mikefischer99@gmail.com>
2011-02-12 22:15:19 -0500

    Optimized stylesheet installation.

--- _stylesheets.rb
+++ _stylesheets.rb
@@ -4,16 +4,11 @@ puts "Creating default stylesheets ...".magenta
 
 remove_file 'app/stylesheets/partials/_example.sass'
 
-copy_static_file 'app/stylesheets/partials/_buttons.sass'
-copy_static_file 'app/stylesheets/partials/_flashes.sass'
-copy_static_file 'app/stylesheets/partials/_forms.sass'
-copy_static_file 'app/stylesheets/partials/_tables.sass'
-copy_static_file 'app/stylesheets/partials/_page.sass'
-
 gsub_file 'app/stylesheets/style.sass', %r{//@include html5-boilerplate;}, '@include html5-boilerplate'
 gsub_file 'app/stylesheets/style.sass', %r{@import partials/example}, '//@import partials/example'
 
-append_file 'app/stylesheets/style.sass', "@import partials/buttons\n"
-append_file 'app/stylesheets/style.sass', "@import partials/flashes\n"
-append_file 'app/stylesheets/style.sass', "@import partials/forms\n"
-append_file 'app/stylesheets/style.sass', "@import partials/tables\n"
+%w{page buttons flashes forms tables}.each do |component|
+  copy_static_file "app/stylesheets/partials/_#{component}.sass"
+  append_file "app/stylesheets/style.sass", "@import partials/#{component}\n"
+end
+
